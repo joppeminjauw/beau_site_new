80057988cc9aed920ad3a37da54e5f39dbc29490
Joppe Minjauw <joppe.minjauw@gmail.com>
2021-01-18 20:11:05 +0100

    slideshow + layout changes

--- js/beau.js
+++ js/beau.js
@@ -50,16 +50,45 @@ activateSection = () => {
 
     sections.each(function () {
 
-        var top = $(this).offset().top-120;
+        var top = $(this).offset().top - 120;
         var bottom = top + $(this).outerHeight();
 
         if (pos >= top && pos <= bottom) {
             var id = $(this).attr('id');
 
-            if (id) {   
+            if (id) {
                 nav.find('a').removeClass('active');
                 nav.find('a[href="#' + id + '"]').addClass('active');
             }
         }
     });
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activeDot", " ");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " activeDot";
 }
